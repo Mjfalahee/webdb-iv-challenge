1290f3650d6ce4166e8cbdd9b3345523855eb552
mjfalahee <mjfalahee@gmail.com>
2019-06-06 18:51:21 -0700

    Most of MVP is done, still working on getDishbyID

--- data/models/recipeModel.js
+++ data/models/recipeModel.js
@@ -5,7 +5,8 @@ module.exports = {
     addDish,
     getDish,
     getRecipes,
-    addRecipe
+    addRecipe,
+    getRecipesByDishId
 }
 
 //should return a list of all dishes in the database.
@@ -19,20 +20,28 @@ function addDish(dish) {
         .insert(dish);
 };
 
+function getRecipesByDishId(id) {
+    return db
+        .select(['recipes.name'])
+        .from('recipes')
+        .where('recipes.dish_id', id);
+}
+
 //should return the dish with the provided id, and include a list of the related recipes.
 function getDish(id) {
     return db
-        // .select(['recipes.id', 'recipes.name', 'dishes.name as dishes'])
-        // .from('dishes')
-        // .join('recipes', 'recipes.dish_id', 'dishes.id')
-        // .where('dishes.id', id);
+        .select(['dishes.name'])
+        .from('dishes')
+        .where('dishes.id', id)
+        .then(getRecipesByDishId(id));
 };
 
 //should return a list of all recipes in the database, including the dish they belong to.
 function getRecipes() {
     return db
-        .select()
-
+        .select(['recipes.name', 'dishes.name'])
+        .from('recipes')
+        .join('dishes', 'dishes.id', 'recipes.dish_id')
 };
 
 //should add a recipe to the database and return the id of the new recipe.
@@ -40,3 +49,8 @@ function addRecipe(recipe) {
     return db('recipes')
         .insert(recipe);
 };
+
+//takes a recipe id, and gives a list of ingredients and their quantities to buy
+function getShoppingList(id) {
+
+}
--- data/routers/recipeRouter.js
+++ data/routers/recipeRouter.js
@@ -3,3 +3,64 @@ const router = express.Router();
 
 const db = require('../models/recipeModel');
 
+// getDishes,
+// addDish,
+// getDish,
+// getRecipes,
+// addRecipe
+
+//get dishes == works
+
+function sendError(status, message) {
+    res.status(status).json({errorMessage: message});
+}
+
+router.get('/dishes', async (req, res) => {
+    try {
+        const dishes = await db.getDishes();
+        res.status(200).json(dishes);
+    }
+    catch (error) {
+       sendError(500, 'Error grabbing dishes');
+    }
+})
+
+//add dish == works
+
+router.post('/dishes', async (req, res) => {
+    try {
+        const dish = await db.addDish(req.body);
+        res.status(200).json(dish);
+    }
+    catch (error) {
+        sendError(500, 'Error creating dish.');
+    }
+})
+
+//get dish by id
+
+router.get('/dishes/:id', async (req, res) => {
+    try {
+        const dish = await db.getDish(req.params.id);
+        //const recipe = await db.getRecipesByDishId(req.params.id);
+        //console.log(dish);
+        //console.log(recipe);
+        // obj = {
+        //     name: dish,
+        //     recipes: recipe
+        // }
+        res.status(200).json(dish);
+    }
+    catch (error) {
+        sendError(500, 'Error getting the Dish.')
+    }
+})
+
+//get recipes
+
+//add recipes
+
+//get shopping list
+
+
+module.exports = router;
